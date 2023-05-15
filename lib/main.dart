d67bc3227eda05e09c992aa21a36103c5010980c
AdamMbule <adammbule98@gmail.com>
2023-05-15 23:44:44 +0300

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() {runApp(const AppBar());};
+
+class AppBar extends StatelessWidget {
+  const AppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AppBarExample(),
+    );
+  }
 }
 
+class 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -14,7 +24,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Apprentice',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
@@ -35,9 +45,12 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
+      AppBar: AppBar(
+        title: const Text('Apprentice');
+      );
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random idea:Piratecoin'),
           Text(appState.current.asLowerCase),
         ],
       ),

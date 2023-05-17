903de0f7a88b1614bd68f26c59205dfb8d47837f
AdamMbule <adammbule98@gmail.com>
2023-05-17 22:54:49 +0300

    Widgetry

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,12 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() {runApp(const AppBar());};
-
-class AppBar extends StatelessWidget {
-  const AppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: AppBarExample(),
-    );
-  }
-}
-
-class 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Apprentice',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
+void main(){
+  runApp(
+    MaterialApp(
+      home:Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
       ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      AppBar: AppBar(
-        title: const Text('Apprentice');
-      );
-      body: Column(
-        children: [
-          Text('A random idea:Piratecoin'),
-          Text(appState.current.asLowerCase),
-        ],
-      ),
-    );
-  }
+    ),
+  );
 }

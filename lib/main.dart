9aa2953898036068d0eb788e74d723fcb62fb72e
AdamMbule <adammbule98@gmail.com>
2023-05-17 23:02:24 +0300

    HOt reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-    MaterialApp(
+  runApp(Piratecoin()    
+  );
+}
+
+class Piratecoin extends StatelessWidget {
+Piratecoin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home:Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }

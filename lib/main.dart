31f81408669526731de74cc1993d5170dcf85ec3
AdamMbule <adammbule98@gmail.com>
2023-05-30 23:06:37 +0300

    Welcome page navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,111 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Piratecoin()    
-  );
+void main() {
+  runApp(Piratecoin());
 }
 
 class Piratecoin extends StatelessWidget {
-Piratecoin({super.key});
+  Piratecoin({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.redAccent,
           title: Text(
             'Piratecoin',
             style: TextStyle(
-                    fontFamily: 'Roboto', //obtain fonts from fonts.google.com
-                    fontSize: 30.0,
-                    color: Colors.black45,
-                    fontWeight: FontWeight.bold,
-                    //learn to center the text
-                  ),
-                  ),
-                  
+              fontFamily: 'Roboto', //obtain fonts from fonts.google.com
+              fontSize: 30.0,
+              color: Colors.black45,
+              fontWeight: FontWeight.bold,
+              //learn to center the text
+            ),
+          ),
         ),
         backgroundColor: Colors.lightBlue,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(//container
-            height:75.0,
-            width:double.infinity,
-            color:Colors.white,
-            child:Text('Enter Username'),
-            margin:EdgeInsets.only(left: 30.0),
-            padding:EdgeInsets.all(20.0), 
-              //fix the code Container(// add icons
-            /*child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  //container
+                  height: 75.0,
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Text('Enter Username'),
+                  margin: EdgeInsets.only(left: 30.0),
+                  padding: EdgeInsets.all(20.0),
+                  //fix the code Container(// add icons
+                  /*child: Row(
                   children: <Widget>[
                     Icon(Icons.login_rounded,
                     size: 50.0,
                     color: Colors.black54),
                   ],
                 ),*/
-                ),                           
-            
-              SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                width:double.infinity,
-                height:75,
-                color: Colors.white,
-                margin:EdgeInsets.only(left: 30.0),
-                padding:EdgeInsets.all(20.0),
-                child:Text('Enter Password'),
-                                            
-              ), //container 2
-              SizedBox(
-                height: 30.0,
-              ),
-              Text('Forgot Your Password?'),
+                ),
 
-              /*Expanded(
+                SizedBox(
+                  height: 30.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 75,
+                  color: Colors.white,
+                  margin: EdgeInsets.only(left: 30.0),
+                  padding: EdgeInsets.all(20.0),
+                  child: Text('Enter Password'),
+                ), //container 2
+                SizedBox(
+                  height: 30.0,
+                ),
+                Text('Forgot Your Password?'),
+
+                /*Expanded(
             child: FloatingActionButton( 
               onPressed: (){
                 print('Login Successful');
                 }),),*/
-              
-              Container(// add icons
-                width:double.infinity,
-                height:75,
-                child: FloatingActionButton(
-                  backgroundColor: Colors.black,
-                  onPressed: (){
-                    print('Login Successful....');
-                  },
-                  /*child: Row(
+
+                Container(
+                  // add icons
+                  width: double.infinity,
+                  height: 75,
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.black,
+                    onPressed: () {
+                      print('Login Successful....');
+                    },
+                    /*child: Row(
                     children: <Widget>[
                     Icon(Icons.login_outlined,
                     size: 50.0,
                     color: Color.fromARGB(255, 15, 3, 2)),
                   ],
                 ),*/
-                ), 
-
-              ), //To be implemented later
-              /*CircleAvatar( //to be deployed later
+                  ),
+                ), //To be implemented later
+                /*CircleAvatar( //to be deployed later
                 Radius: 50.0,
                 backgroundImage:AssetImage('images/'), //Don't forget to edit pubspec.yaml
-              ), */               
-        ]
-            ),
+              ), */
+              ]),
         ),
-        )
+      ),
     );
-      }
+  }
 }
 
+class WelcomePage extends StatelessWidget { //Welcome page navigation
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Trending Piratecoins'),
+      ),
+      body: // Add your page content here
+    );
+  }
+}
